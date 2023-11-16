e18e83a00941dd79a766397ae5d49c7fcd803192
demianchuk-o <demyanchukua@gmail.com>
2023-11-16 18:50:31 +0200

    Created WebApi project and BooksController

---
+++ LibrarySystem.WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,40 @@
+﻿using LibrarySystem.Common.Books;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystem.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<BookDto>>> GetAll()
+    {
+        return Ok(new List<BookDto>());
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<BookDto>> GetById(int id)
+    {
+        return Ok(new BookDto("title", "author", "subject", true));
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult> Create([FromBody] BookCreatedDto book)
+    {
+        var bookEntity = new BookDto(book.Author, book.Title, book.Subject, true);
+        return CreatedAtAction(nameof(GetById), new { id = 1 }, bookEntity);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Update(int id, [FromBody] BookUpdatedDto book)
+    {
+        return Ok(book);
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        return NoContent();
+    }
+}
