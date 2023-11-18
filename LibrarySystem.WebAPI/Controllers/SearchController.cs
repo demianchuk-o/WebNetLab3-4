36491284667fdeb7fc604029a0f1b723f475c780
demianchuk-o <demyanchukua@gmail.com>
2023-11-18 17:07:14 +0200

    Designed SearchController for all website visitors to search through book catalogue

---
+++ LibrarySystem.WebAPI/Controllers/SearchController.cs
@@ -0,0 +1,18 @@
+﻿using LibrarySystem.Common.Books;
+using LibrarySystem.Common.Search;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystem.WebAPI.Controllers;
+
+[AllowAnonymous]
+[ApiController]
+[Route("api/[controller]")]
+public class SearchController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<BookDto>>> GetSearchResults([FromQuery] SearchQueryDto query, PaginationDto pagination)
+    {
+        return Ok(new List<BookDto>());
+    }
+}
