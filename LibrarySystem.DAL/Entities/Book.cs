05ee0e49061000fba05eb2d9c69dcc90b86a2f91
demianchuk-o <demyanchukua@gmail.com>
2023-12-09 11:47:04 +0200

    defined Book entity with Author and Subject helper entities

---
+++ LibrarySystem.DAL/Entities/Book.cs
@@ -0,0 +1,14 @@
+﻿using LibrarySystem.DAL.Entities.Abstract;
+
+namespace LibrarySystem.DAL.Entities;
+
+public class Book : IIdentity
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public Guid AuthorId { get; set; }
+    public Author Author { get; set; }
+    public Guid SubjectId { get; set; }
+    public Subject Subject { get; set; }
+    public bool Availability { get; set; } = true;
+}
