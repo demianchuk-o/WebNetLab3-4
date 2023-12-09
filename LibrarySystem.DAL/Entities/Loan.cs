bdef1248b7c7701fb3441ceba1d1dd868c41b0a4
demianchuk-o <demyanchukua@gmail.com>
2023-12-09 12:24:46 +0200

    installed AspNetCore.Identity package

---
+++ LibrarySystem.DAL/Entities/Loan.cs
@@ -0,0 +1,15 @@
+﻿using LibrarySystem.DAL.Entities.Abstract;
+
+namespace LibrarySystem.DAL.Entities;
+
+public class Loan : IIdentity
+{
+    public Guid Id { get; set; }
+    public Guid BorrowerId { get; set; }
+    public User Borrower { get; set; }
+    public DateTime BorrowDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ReturnDate { get; set; } = null;
+    
+    public ICollection<Book> Books { get; init; } = new List<Book>();
+}
