2717840306cc09ba05fc7f36108edbfe5eae15af
demianchuk-o <demyanchukua@gmail.com>
2023-12-11 20:42:04 +0200

    Implemented basic Specification pattern

---
+++ LibrarySystem.DAL/Specifications/Abstract/Specification.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using LibrarySystem.DAL.Entities.Abstract;
+
+namespace LibrarySystem.DAL.Specifications.Abstract;
+
+public abstract class Specification<TEntity>
+    where TEntity : class, IIdentity
+{
+    public Expression<Func<TEntity, bool>>? Criteria { get; }
+    
+    protected Specification(Expression<Func<TEntity, bool>>? criteria)
+    {
+        Criteria = criteria;
+    }
+    public List<Expression<Func<TEntity, object>>> Includes { get; } = new();
+    
+    protected void AddInclude(Expression<Func<TEntity, object>> includeExpression)
+    {
+        Includes.Add(includeExpression);
+    }
+}
---
+++ LibrarySystem.DAL/Specifications/SpecificationEvaluator.cs
@@ -0,0 +1,25 @@
+﻿using LibrarySystem.DAL.Entities.Abstract;
+using LibrarySystem.DAL.Specifications.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibrarySystem.DAL.Specifications;
+
+public static class SpecificationEvaluator
+{
+    public static IQueryable<TEntity> GetQuery<TEntity>(IQueryable<TEntity> inputQuery, Specification<TEntity> specification)
+        where TEntity : class, IIdentity
+    {
+        var query = inputQuery;
+        if (specification.Criteria is not null)
+        {
+            query = query.Where(specification.Criteria);
+        }
+
+        query = specification.Includes.
+            Aggregate(query, 
+                (current, includeExpression) => 
+                    current.Include(includeExpression));
+
+        return query;
+    }
+}
