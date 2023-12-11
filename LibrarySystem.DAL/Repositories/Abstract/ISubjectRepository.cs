d4dacf982722ef5b2a09408f9288d587f26ee233
demianchuk-o <demyanchukua@gmail.com>
2023-12-11 19:17:19 +0200

    Created repository interfaces for Author and Subject entities

---
+++ LibrarySystem.DAL/Repositories/Abstract/ISubjectRepository.cs
@@ -0,0 +1,8 @@
+﻿using LibrarySystem.DAL.Entities;
+
+namespace LibrarySystem.DAL.Repositories.Abstract;
+
+public interface ISubjectRepository : IRepository<Subject>
+{
+    Task<Subject?> GetByNameAsync(string name);
+}
