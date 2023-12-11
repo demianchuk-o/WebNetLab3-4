4eb89f7bad219a6a026dd077c03ceab6caaa2ec0
demianchuk-o <demyanchukua@gmail.com>
2023-12-11 17:20:46 +0200

    Created IUserRepository interface

---
+++ LibrarySystem.DAL/Repositories/Abstract/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using LibrarySystem.DAL.Entities;
+
+namespace LibrarySystem.DAL.Repositories.Abstract;
+
+public interface IUserRepository : IRepository<User>
+{
+    Task<User?> GetByEmailAsync(string email);
+    Task<string> GetRoleNameAsync(User user); 
+}
