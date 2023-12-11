2c9f665b9824c382764ffdfd45a727d654d22a38
demianchuk-o <demyanchukua@gmail.com>
2023-12-11 20:42:44 +0200

    Defined specifications for book queries

---
+++ LibrarySystem.DAL/Specifications/Books/BookByIdWithAuthorAndSubjectSpecification.cs
@@ -0,0 +1,14 @@
+﻿using LibrarySystem.DAL.Entities;
+using LibrarySystem.DAL.Specifications.Abstract;
+
+namespace LibrarySystem.DAL.Specifications.Books;
+
+public class BookByIdWithAuthorAndSubjectSpecification : Specification<Book>
+{
+    public BookByIdWithAuthorAndSubjectSpecification(Guid id) 
+        : base(b => b.Id == id)
+    {
+        AddInclude(book => book.Author);
+        AddInclude(book => book.Subject);
+    }
+}
