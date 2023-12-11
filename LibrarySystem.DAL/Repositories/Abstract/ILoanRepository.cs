bbcde5ed0ab4fafe7fc82e27d7e2e8787876fcc1
demianchuk-o <demyanchukua@gmail.com>
2023-12-11 17:23:59 +0200

    Created ILoanRepository interface

---
+++ LibrarySystem.DAL/Repositories/Abstract/ILoanRepository.cs
@@ -0,0 +1,9 @@
+﻿using LibrarySystem.DAL.Entities;
+
+namespace LibrarySystem.DAL.Repositories.Abstract;
+
+public interface ILoanRepository : IRepository<Loan> 
+{
+    Task<IEnumerable<Loan>> GetAllWithUserAndBooksAsync();
+    Task<Loan?> GetByIdWithUserAndBooksAsync(Guid id);
+}
