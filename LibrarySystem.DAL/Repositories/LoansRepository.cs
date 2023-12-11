69790bb6692c8ea274731ae8fd461f36b84df6f3
demianchuk-o <demyanchukua@gmail.com>
2023-12-11 20:44:06 +0200

    Implemented LoansRepository

---
+++ LibrarySystem.DAL/Repositories/LoansRepository.cs
@@ -0,0 +1,25 @@
+﻿using LibrarySystem.DAL.Entities;
+using LibrarySystem.DAL.Repositories.Abstract;
+using LibrarySystem.DAL.Specifications.Loans;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibrarySystem.DAL.Repositories;
+
+public class LoansRepository : Repository<Loan>, ILoanRepository
+{
+    public LoansRepository(DbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Loan>> GetAllWithUserAndBooksAsync()
+    {
+        return await ApplySpecification(new AllLoansWithUserAndBooksSpecification())
+            .ToListAsync();
+    }
+
+    public async Task<Loan?> GetByIdWithUserAndBooksAsync(Guid id)
+    {
+        return await ApplySpecification(new LoanByIdWIthUserAndBooksSpecification(id))
+            .FirstOrDefaultAsync();
+    }
+}
