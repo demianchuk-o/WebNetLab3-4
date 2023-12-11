1d04c4f5d8249f4ad6e5a1eacab2ef9e5fcdf7c4
demianchuk-o <demyanchukua@gmail.com>
2023-12-11 17:08:25 +0200

    implemented generic repository interface and base class

---
+++ LibrarySystem.DAL/Repositories/Abstract/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using LibrarySystem.DAL.Entities.Abstract;
+
+namespace LibrarySystem.DAL.Repositories.Abstract;
+
+public interface IRepository<TEntity> 
+    where TEntity : class, IIdentity
+{
+    Task<TEntity?> GetByIdAsync(Guid id);
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task AddAsync(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    Task DeleteByIdAsync(Guid id);
+}
---
+++ LibrarySystem.DAL/Repositories/Abstract/Repository.cs
@@ -0,0 +1,50 @@
+﻿using LibrarySystem.DAL.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibrarySystem.DAL.Repositories.Abstract;
+
+public abstract class Repository<TEntity> : IRepository<TEntity>
+    where TEntity : class, IIdentity
+{
+    protected readonly DbSet<TEntity> DbSet;
+
+    protected Repository(DbContext context)
+    {
+        DbSet = context.Set<TEntity>();
+    }
+
+    public async Task<TEntity?> GetByIdAsync(Guid id)
+    {
+        return await DbSet.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await DbSet.ToListAsync();
+    }
+
+    public async Task AddAsync(TEntity entity)
+    {
+        await DbSet.AddAsync(entity);
+    }
+
+    public void Update(TEntity entity)
+    {
+        DbSet.Update(entity);
+    }
+
+    public void Delete(TEntity entity)
+    {
+        DbSet.Remove(entity);
+    }
+
+    public async Task DeleteByIdAsync(Guid id)
+    {
+        var entity = await DbSet.FindAsync(id);
+        
+        if (entity is not null)
+        {
+            DbSet.Remove(entity);
+        }
+    }
+}
