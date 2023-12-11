3c672156d33882b5f997f47cd1e0203fa63a86e9
demianchuk-o <demyanchukua@gmail.com>
2023-12-11 20:43:56 +0200

    Defined loans specifications

---
+++ LibrarySystem.DAL/Specifications/Loans/LoanByIdWIthUserAndBooksSpecification.cs
@@ -0,0 +1,14 @@
+﻿using LibrarySystem.DAL.Entities;
+using LibrarySystem.DAL.Specifications.Abstract;
+
+namespace LibrarySystem.DAL.Specifications.Loans;
+
+internal class LoanByIdWIthUserAndBooksSpecification : Specification<Loan>
+{
+    public LoanByIdWIthUserAndBooksSpecification(Guid id)
+    : base(loan => loan.Id == id)
+    {
+        AddInclude(loan => loan.Borrower);
+        AddInclude(loan => loan.Books);
+    }
+}
