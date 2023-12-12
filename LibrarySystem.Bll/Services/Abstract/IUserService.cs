76d08bd723ef30a72cb3fa499fca59116e786272
demianchuk-o <demyanchukua@gmail.com>
2023-12-12 22:33:39 +0200

    defined IUserService interface

---
+++ LibrarySystem.Bll/Services/Abstract/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using LibrarySystem.Bll.Models;
+
+namespace LibrarySystem.Bll.Services.Abstract;
+
+public interface IUserService : ICrudService<UserModel>
+{
+    Task<UserModel?> GetByEmailAsync(string email);
+    Task<string> AuthenticateAsync(string email, string password);
+}
