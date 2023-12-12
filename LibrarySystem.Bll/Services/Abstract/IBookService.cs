1f39d99cf36ceba094602481023b3595601d63e0
demianchuk-o <demyanchukua@gmail.com>
2023-12-12 22:57:04 +0200

    fix: added parameters for GetSearchResultsAsync method

--- LibrarySystem.Bll/Services/Abstract/IBookService.cs
+++ LibrarySystem.Bll/Services/Abstract/IBookService.cs
@@ -1,8 +1,9 @@
 ﻿using LibrarySystem.Bll.Models;
+using LibrarySystem.Common.Search;
 
 namespace LibrarySystem.Bll.Services.Abstract;
 
 public interface IBookService : ICrudService<BookModel>
 {
-    Task<IEnumerable<BookModel>> GetSearchResultsAsync();
+    Task<IEnumerable<BookModel>> GetSearchResultsAsync(SearchQueryDto searchQuery, PaginationDto pagination);
 }
