c41387b0b12f80b062fa43e1e860703204738e59
demianchuk-o <demyanchukua@gmail.com>
2023-12-12 14:08:00 +0200

    Defined the base service class that declares the usage of repositories

---
+++ LibrarySystem.Bll/Services/Abstract/BaseService.cs
@@ -0,0 +1,15 @@
+﻿using LibrarySystem.DAL.Entities.Abstract;
+using LibrarySystem.DAL.Repositories.Abstract;
+
+namespace LibrarySystem.Bll.Services.Abstract;
+
+public abstract class BaseService<TEntity>
+    where TEntity : class, IIdentity
+{
+    protected readonly IRepository<TEntity> Repository;
+
+    protected BaseService(IRepository<TEntity> repository)
+    {
+        Repository = repository;
+    }
+}
