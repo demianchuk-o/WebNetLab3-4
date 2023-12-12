82a6892d755eeaf108709b8e978e422f560a4a18
demianchuk-o <demyanchukua@gmail.com>
2023-12-12 22:56:45 +0200

    Make Repository a type argument to allow inherited interfaces for repositories in services

--- LibrarySystem.Bll/Services/Abstract/BaseService.cs
+++ LibrarySystem.Bll/Services/Abstract/BaseService.cs
@@ -1,15 +1,19 @@
-﻿using LibrarySystem.DAL.Entities.Abstract;
+﻿using AutoMapper;
+using LibrarySystem.DAL.Entities.Abstract;
 using LibrarySystem.DAL.Repositories.Abstract;
 
 namespace LibrarySystem.Bll.Services.Abstract;
 
-public abstract class BaseService<TEntity>
+public abstract class BaseService<TEntity, TRepository>
     where TEntity : class, IIdentity
+    where TRepository : IRepository<TEntity>
 {
     protected readonly IRepository<TEntity> Repository;
-
-    protected BaseService(IRepository<TEntity> repository)
+    protected readonly IMapper Mapper;
+    
+    protected BaseService(TRepository repository, IMapper mapper)
     {
         Repository = repository;
+        Mapper = mapper;
     }
 }
