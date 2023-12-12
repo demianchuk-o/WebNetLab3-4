748a1a3617bcb9bb71cd05169bb10be86d0f8ac6
demianchuk-o <demyanchukua@gmail.com>
2023-12-12 14:07:25 +0200

    Defined the BookModel

---
+++ LibrarySystem.Bll/Models/BookModel.cs
@@ -0,0 +1,14 @@
+﻿using LibrarySystem.Bll.Models.Abstract;
+
+namespace LibrarySystem.Bll.Models;
+
+public class BookModel : IModel
+{
+    public Guid Id { get; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Subject { get; set; }
+    public bool Availability { get; set; } = true;
+    
+    public bool IsAvailable() => Availability;
+}
