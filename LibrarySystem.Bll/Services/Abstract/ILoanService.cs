29d1a68e8cd0ec05af29135f6e739d9d718ca7f1
demianchuk-o <demyanchukua@gmail.com>
2023-12-12 22:34:48 +0200

    defined ILoanService interface

---
+++ LibrarySystem.Bll/Services/Abstract/ILoanService.cs
@@ -0,0 +1,9 @@
+﻿using LibrarySystem.Bll.Models;
+
+namespace LibrarySystem.Bll.Services.Abstract;
+
+public interface ILoanService : ICrudService<LoanModel>
+{
+    Task MakeLoanAsync(LoanModel model);
+    Task ReturnLoanAsync(Guid id);
+}
