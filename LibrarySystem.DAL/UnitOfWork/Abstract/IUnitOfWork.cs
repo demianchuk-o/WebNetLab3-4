4a0c1954b8f380a15eedd061b8ad7ffd9038638a
demianchuk-o <demyanchukua@gmail.com>
2023-12-13 14:13:18 +0200

    put repositories inside of UnitOfWork

--- LibrarySystem.DAL/UnitOfWork/Abstract/IUnitOfWork.cs
+++ LibrarySystem.DAL/UnitOfWork/Abstract/IUnitOfWork.cs
@@ -1,6 +1,14 @@
-﻿namespace LibrarySystem.DAL.UnitOfWork.Abstract;
+﻿using LibrarySystem.DAL.Repositories.Abstract;
+
+namespace LibrarySystem.DAL.UnitOfWork.Abstract;
 
 public interface IUnitOfWork
 {
+    IBookRepository Books { get; }
+    IAuthorRepository Authors { get; }
+    ISubjectRepository Subjects { get; }
+    ILoanRepository Loans { get; }
+    IUserRepository Users { get; }
+    
     Task<int> SaveChangesAsync();
 }
--- LibrarySystem.DAL/UnitOfWork/UnitOfWork.cs
+++ LibrarySystem.DAL/UnitOfWork/UnitOfWork.cs
@@ -1,4 +1,5 @@
 ﻿using LibrarySystem.DAL.Context;
+using LibrarySystem.DAL.Repositories.Abstract;
 using LibrarySystem.DAL.UnitOfWork.Abstract;
 
 namespace LibrarySystem.DAL.UnitOfWork;
@@ -7,11 +8,20 @@ public class UnitOfWork : IUnitOfWork
 {
     private readonly LibraryDbContext _context;
 
-    public UnitOfWork(LibraryDbContext context)
+    public IBookRepository Books { get; }
+    public IAuthorRepository Authors { get; }
+    public ISubjectRepository Subjects { get; }
+    public ILoanRepository Loans { get; }
+    public IUserRepository Users { get; }
+    public UnitOfWork(LibraryDbContext context, IBookRepository books, IAuthorRepository authors, ISubjectRepository subjects, ILoanRepository loans, IUserRepository users)
     {
         _context = context;
+        Books = books;
+        Authors = authors;
+        Subjects = subjects;
+        Loans = loans;
+        Users = users;
     }
-
 
     public async Task<int> SaveChangesAsync()
     {
