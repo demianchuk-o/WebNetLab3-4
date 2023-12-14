54f048867f8e51e8025717724866f88bcb77ba0f
demianchuk-o <demyanchukua@gmail.com>
2023-12-14 21:06:32 +0200

    implemented UsersController

--- LibrarySystem.WebAPI/Controllers/UsersController.cs
+++ LibrarySystem.WebAPI/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-﻿using LibrarySystem.Common.Users;
+﻿using AutoMapper;
+using LibrarySystem.Bll.Models;
+using LibrarySystem.Bll.Services.Abstract;
+using LibrarySystem.Common.Users;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,28 +12,41 @@ namespace LibrarySystem.WebAPI.Controllers;
 [Route("api/[controller]")]
 public class UsersController : ControllerBase
 {
+    private readonly IUserService _userService;
+    private readonly IMapper _mapper;
+
+    public UsersController(IUserService userService, IMapper mapper)
+    {
+        _userService = userService;
+        _mapper = mapper;
+    }
+
     [HttpGet]
     public async Task<ActionResult<IEnumerable<UserDto>>> GetAll()
     {
-        return Ok(new List<UserDto>());
+        var users = await _userService.GetAllAsync();
+        return _mapper.Map<IEnumerable<UserDto>>(users).ToList();
     }
     
     [HttpGet("{id}")]
     public async Task<ActionResult<UserDto>> GetById(Guid id)
     {
-        return Ok(new UserDto(id, "username@mail.com", "username"));
+        var user = _userService.GetByIdAsync(id);
+        return _mapper.Map<UserDto>(user);
     }
     
     [HttpPut]
     public async Task<ActionResult> Update(Guid id, [FromBody] UserDto user)
     {
-        var userEntity = new UserDto(id, user.Email, user.Username);
+        await _userService.UpdateAsync(id, _mapper.Map<UserModel>(user));
+        var userEntity = _mapper.Map<UserDto>(await _userService.GetByIdAsync(id));
         return Ok(userEntity);
     }
     
     [HttpDelete("{id}")]
     public async Task<ActionResult> Delete(Guid id)
     {
+        await _userService.DeleteByIdAsync(id);
         return NoContent();
     }
 }
