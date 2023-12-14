d78125db11c58b3f7b961216b6a3e8a00edaf0a4
demianchuk-o <demyanchukua@gmail.com>
2023-12-14 20:38:30 +0200

    implemented AuthController

--- LibrarySystem.WebAPI/Controllers/AuthController.cs
+++ LibrarySystem.WebAPI/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using LibrarySystem.Common.Users;
+﻿using AutoMapper;
+using LibrarySystem.Bll.Models;
+using LibrarySystem.Bll.Services.Abstract;
+using LibrarySystem.Common.Users;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibrarySystem.WebAPI.Controllers;
@@ -7,22 +10,44 @@ namespace LibrarySystem.WebAPI.Controllers;
 [Route("api/[controller]")]
 public class AuthController : ControllerBase
 {
+    private readonly IUserService _userService;
+    private readonly IMapper _mapper;
+
+    public AuthController(IUserService userService, IMapper mapper)
+    {
+        _userService = userService;
+        _mapper = mapper;
+    }
+
     [HttpPost("register")]
     public async Task<ActionResult> Register([FromBody] UserRegisterDto user)
     {
-        return CreatedAtAction(nameof(Register), new { id = Guid.NewGuid() }, user);
+        await _userService.RegisterAsync(_mapper.Map<UserModel>(user), user.Password);
+        var userRegistered = await _userService.GetByEmailAsync(user.Email);
+        return CreatedAtAction(nameof(Register), new { id = userRegistered.Id }, userRegistered);
     }
 
     [HttpPost("login")]
     public async Task<ActionResult> Login([FromBody] UserLoginDto user)
     {
-        var userEntity = new UserDto(Guid.NewGuid(), user.Email, "username");
-        return Ok(userEntity);
+        var token = await _userService.AuthenticateAsync(user.Email, user.Password);
+        var userEntity = await _userService.GetByEmailAsync(user.Email);
+
+        if (token is not null)
+        {
+            Response.Headers.Add("Authorization", $"Bearer {token}");
+            
+            return Ok(_mapper.Map<UserDto>(userEntity));
+        }
+        else
+        {
+            return Unauthorized();
+        }
     }
 
     [HttpPost("logout")]
     public async Task<ActionResult> Logout()
     {
-        return NoContent();
+        return Ok(new { message = "Logged out successfully" });
     }
 }
